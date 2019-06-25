2c8f1ae8c893cf99fa37f23e0b62b4a4915694b4
杨佳 <ibambooes@live.cn>
2019-06-25 09:57:30 +0800

    更新

--- src/assets/js/latestTravel.js
+++ src/assets/js/latestTravel.js
@@ -1,12 +1,12 @@
 const latestTravel = [
-  // {
-  //   id: '20190620-1',
-  //   type: 'travel',
-  //   text: '青城 · 问道',
-  //   img: 'home-1',
-  //   detailImg: 'detail-1',
-  //   time: '2019-06-20'
-  // }
+  {
+    id: '20190620-1',
+    type: 'travel',
+    text: '青城 · 问道',
+    img: 'home20190607-1',
+    detailImg: 'detail20190607-1',
+    time: '2019-06-20'
+  }
 ]
 
 export default latestTravel
--- src/assets/js/travelDetails.js
+++ src/assets/js/travelDetails.js
@@ -1,18 +1,18 @@
 const travelDetails = {
   '20190620-1': {
-    title: '你还未来 怎敢老去',
+    title: '青城 · 问道',
     contents: {
       p1: {
         title: '标题一',
         type: 'text-left',
         content: '段很低哦洒几滴哦去哪都去哪成交额我把你从俄进口琪娃能打开吗琪娃的看起来我们农村建设',
-        image: 'detail-1'
+        image: 'detail20190607-1'
       },
       p2: {
         title: '标题二',
         type: 'text-right',
         content: '段很低哦洒几滴哦去哪都去哪成交额我把你从俄进口琪娃能打开吗琪娃的看起来我们农村建设',
-        image: 'detail-2'
+        image: 'detail20190607-1'
       },
       p3: {
         title: '标题三',
@@ -23,7 +23,7 @@ const travelDetails = {
         title: '标题四',
         type: 'image',
         content: '段很低哦洒几滴哦去哪都去哪成交额我把你从俄进口琪娃能打开吗琪娃的看起来我们农村建设',
-        image: 'detail-1'
+        image: 'detail20190607-1'
       }
     }
   }
